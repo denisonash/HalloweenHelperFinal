5f135c57482e636a6691c0cbc65c7680b2cbb7b7
Ashley Denison <denisonash@gmail.com>
2022-10-12 14:40:46 -0600

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -20,4 +20,4 @@ npm install mysql
 npm install ejs
 
 #start our node app in the background
-node index.js > app.out.log 2> app.err.log < /dev/null & 
+node app.js > app.out.log 2> app.err.log < /dev/null & 
