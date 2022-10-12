55e97c9c1a05306d7dcfebed9133632de2510e1c
Ashley Denison <denisonash@gmail.com>
2022-10-12 15:02:54 -0600

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -11,13 +11,15 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
-#install node modules
-npm install
-
-npm install express
 npm install knex
 npm install mysql
 npm install ejs
+
+#install node modules
+npm install
+
+
+
 
 #start our node app in the background
 node app.js > app.out.log 2> app.err.log < /dev/null & 
